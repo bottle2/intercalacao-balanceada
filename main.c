f2bc040ea4f23a3b9c4fcc87a627df55fd41bcf8
Bento Borges Schirmer <bento.schirmer@gmail.com>
2021-07-13 09:56:55 -0300

    Minor cleanup

--- main.c
+++ main.c
@@ -26,7 +26,6 @@ static void merge_sort(FileQueue* queue){
         FILE* aux = fopen(fname, "w+b"); 
         FILE* f1  = filePop(queue);
         FILE* f2  = filePop(queue);
-        FILE* ctr = NULL;
 
         rewind(f1); rewind(f2);
 
@@ -34,7 +33,6 @@ static void merge_sort(FileQueue* queue){
 
         int e1 = 0; 
         int e2 = 0;
-#if 1
         int ex = 0;
         fread(&e1, sizeof(e1), 1, f1);
         fread(&e2, sizeof(e2), 1, f2);
@@ -58,65 +56,6 @@ static void merge_sort(FileQueue* queue){
 
             fwrite(&ex, sizeof(int), 1, aux);
         }
-#else
-        int* ex = NULL;
-        while(fread(&e1, sizeof(e1), 1, f1) != 0 && fread(&e2, sizeof(e2), 1, f2) != 0)
-        {
-            if(e1 > e2)
-            {
-                ex = &e2;
-                fseek(f1,-4,SEEK_CUR);
-            }
-            else
-            {
-                ex = &e1;
-                fseek(f2,-4,SEEK_CUR);
-            }
-            fwrite(ex, sizeof(int), 1, aux);
-            printf("%i %i %i\n", e1, e2 , *ex);
-        }
-
-#if 0
-        if(!feof(f1))
-        {
-            //fseek(f1,-4,SEEK_CUR);
-            ctr = f1;
-            ex = &e1;
-        }
-	else if(!feof(f2))
-        {
-            //fseek(f2,-4,SEEK_CUR);
-            ctr = f2;
-            ex = &e2;
-        }
-	else
-	{
-            assert(!"Deu pau");
-	}
-#else
-        if (0 == bytes1)
-        {
-            fseek(f2,-4,SEEK_CUR);
-            ctr = f2;
-            ex = &e2;
-
-        }
-        else
-        {
-            fseek(f1,-4,SEEK_CUR);
-            ctr = f1;
-            ex = &e1;
-        }
-#endif
-
-        int e = *ex; 
-        do
-        {
-            printf("lele");
-            fwrite(&e, sizeof(int), 1, aux);
-        }
-        while(fread(&e, sizeof(e), 1, ctr) != 0);
-#endif
         
         fclose(f1);
         fclose(f2);
@@ -138,14 +77,14 @@ int main(int argc, char *argv[])
 
     arquivo_original = fopen(argv[1], "rb");
 
-    for (int run_i = 0; !feof(arquivo_original); run_i++)
+    while (!feof(arquivo_original))
     {
         int buffer[MAX_INTS] = {0};
         if (fread(&buffer, sizeof(*buffer), sizeof(buffer) / sizeof(*buffer), arquivo_original))
         {        
             qsort(&buffer, MAX_INTS, sizeof(*buffer), cmp);
             char fname[100000];
-            sprintf(fname, "%s %c %d", PATH_FILES,'A', run_i);
+            sprintf(fname, "%s %c %d", PATH_FILES,'A', getQueueNewId(file_queue));
             FILE* fp = fopen(fname, "w+b");
             fwrite(&buffer, sizeof(*buffer), MAX_INTS, fp);
             filePush(file_queue, fp);
